d846f00647ca45bccbaf96466ac89c8d4143f8b1
adrian lozada cabrera <alozada.smartsoft@gmail.com>
2022-04-13 17:40:37 -0500

    se agrego el navigation y los fragments

--- app/src/main/java/com/smartsoft/foodymvvm/MainActivity.kt
+++ app/src/main/java/com/smartsoft/foodymvvm/MainActivity.kt
@@ -2,10 +2,34 @@ package com.smartsoft.foodymvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.smartsoft.foodymvvm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navController = findNavController(R.id.navHostFragment)
+        val appBarConfig = AppBarConfiguration(
+            setOf(
+                R.id.recetasFragment,
+                R.id.recetasFavoritasFragment,
+                R.id.foodJokerFragment
+            )
+        )
+        binding.bottomNavigationView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfig)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
